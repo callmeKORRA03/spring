bf7e2804ea7f8d0a13ee845ea71d10971fb498e6
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-13 00:02:13 +0100

    Add module 3 example main class

---
+++ examples/module-03/layered-app-demo/src/main/java/com/example/layeredapp/LayeredAppDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.layeredapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LayeredAppDemoApplication.class, args);
+    }
+}

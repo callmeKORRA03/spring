a06781ada1ab3799016ebfce702c356a66f878f0
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-13 00:09:30 +0100

    Add module 4 example main class

---
+++ examples/module-04/student-crud-demo/src/main/java/com/example/studentcrud/StudentCrudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.studentcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCrudDemoApplication.class, args);
+    }
+}

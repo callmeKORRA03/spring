ccebdc0ebb8a67193df912bd0f0530c849d0f861
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-13 00:23:29 +0100

    Add module 5 example CourseController

---
+++ examples/module-05/api-quality-demo/src/main/java/com/example/apiquality/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.example.apiquality.controller;
+
+import com.example.apiquality.model.Course;
+import com.example.apiquality.service.CourseService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.createCourse(course));
+    }
+}

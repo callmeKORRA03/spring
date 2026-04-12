267b7b25a7aec5bdf29b7adcd44a655e7caf78c5
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-13 00:31:51 +0100

    Add module 6 example main class

---
+++ examples/module-06/security-starter-demo/src/main/java/com/example/securitystarter/SecurityStarterDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.securitystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStarterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityStarterDemoApplication.class, args);
+    }
+}

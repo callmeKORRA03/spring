5824b14f0809c5fb648e3c32279cf5fcb77925dd
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-12 23:55:42 +0100

    Add module 2 example main class

---
+++ examples/module-02/web-core-demo/src/main/java/com/example/webcore/WebCoreDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.webcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCoreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCoreDemoApplication.class, args);
+    }
+}

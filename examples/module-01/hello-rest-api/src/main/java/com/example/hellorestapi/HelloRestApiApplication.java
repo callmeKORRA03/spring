b906dc3c9bc66488ded96175e5593347d5fa2434
Chibuike Favour <116469228+callmeKORRA03@users.noreply.github.com>
2026-04-12 23:49:21 +0100

    Add example application main class

---
+++ examples/module-01/hello-rest-api/src/main/java/com/example/hellorestapi/HelloRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRestApiApplication.class, args);
+    }
+}
